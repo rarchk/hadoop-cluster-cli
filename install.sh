ec1d960cb6650ce3666f8b6d71a84853f638d0d7
rarchk <tau.ronak@gmail.com>
2014-08-27 02:59:18 +0530

    second commit

--- install.sh
+++ install.sh
@@ -7,6 +7,31 @@
 # Created: Aug 26, 2014
 #
 # 
+# The directory at which hadoop will be installed.
+ HADOOP_HOME=/opt/hadoop
+
+# The username of the dedicated hadoop-installation user
+ HADOOP_USER=hadoop
+
+# If set to 0, then the user login will be disabled. You will be able to connect
+# only through ssh (without password), after setting up the ssh-keys 
+# appropriately. If set to any other value, then the script will require you
+# to type in a password for the new user.
+ HADOOP_USER_ENABLE_LOGIN=0
+
+# The group name of the dedicated hadoop-installation user
+ HADOOP_GROUP=hadoop
+
+# Directory to be used for the hadoop file system
+ HADOOP_HDFS=/app/hadooo/tmp-${HADOOP_USER}
+
+# Name of the hadoop version you are installing (doesn't really matter)
+ HADOOP_VERSION_NAME=2.4.1
+
+# The temporary directory used to build hadoop
+ HADOOP_TMP=/tmp/hadoop_build_$(date +%Y%m%d%H%M%S)
+
+ JAVA_HOME=usr/lib/jvm/jre-1.7.0
 
 # The directory at which hadoop will be installed.
 # Installation Process Output Log File
